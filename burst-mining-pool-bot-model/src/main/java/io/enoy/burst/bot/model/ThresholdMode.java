15becb7fbb6dca0733b0b9ad7ad26a6dd223a76c
enoy <enoy19@gmail.com>
2018-07-07 15:47:37 +0200

    added default threshold constant

--- burst-mining-pool-bot-model/src/main/java/io/enoy/burst/bot/model/ThresholdMode.java
+++ burst-mining-pool-bot-model/src/main/java/io/enoy/burst/bot/model/ThresholdMode.java
@@ -5,6 +5,8 @@ public enum ThresholdMode {
 	SINGLE("Notifications will be sent when a single pending payment increase is greater than your threshold"),
 	PAYOUT("Works like accumulate but only counts payouts (pending payment decreases).");
 
+	public static final ThresholdMode DEFAULT_THRESHOLD_MODE = ACCUMULATE;
+
 	private final String helpText;
 
 	ThresholdMode(String helpText) {
