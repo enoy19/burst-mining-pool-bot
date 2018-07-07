b78be7a26f24e990a6ed59634139163f87b6400a
enoy <enoy19@gmail.com>
2018-07-07 15:51:13 +0200

    added default suffix to help text

--- burst-mining-pool-bot-model/src/main/java/io/enoy/burst/bot/model/ThresholdMode.java
+++ burst-mining-pool-bot-model/src/main/java/io/enoy/burst/bot/model/ThresholdMode.java
@@ -2,7 +2,7 @@ package io.enoy.burst.bot.model;
 
 public enum ThresholdMode {
 	ACCUMULATE("Adds all incrementations of your pending payment and notifies you when the amount is greater than your threshold."),
-	SINGLE("Notifications will be sent when a single pending payment increase is greater than your threshold"),
+	SINGLE("Notifications will be sent when a single pending payment increase is greater than your threshold."),
 	PAYOUT("Works like accumulate but only counts payouts (pending payment decreases).");
 
 	public static final ThresholdMode DEFAULT_THRESHOLD_MODE = ACCUMULATE;
@@ -14,7 +14,7 @@ public enum ThresholdMode {
 	}
 
 	public String getHelpText() {
-		return helpText;
+		return helpText + (this.equals(DEFAULT_THRESHOLD_MODE) ? " (Default)" : "");
 	}
 
 }
