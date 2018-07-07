3007046b1ded5ea3eb98947661d18d819f3e9a88
enoy <enoy19@gmail.com>
2018-07-07 19:40:33 +0200

    made debug increase configurable
    default: 10

--- burst-mining-pool-bot-data-gatherer/src/main/java/io/enoy/burst/bot/data/gatherer/DebugDataGatherer.java
+++ burst-mining-pool-bot-data-gatherer/src/main/java/io/enoy/burst/bot/data/gatherer/DebugDataGatherer.java
@@ -5,6 +5,7 @@ import io.enoy.burst.bot.controller.service.WalletService;
 import io.enoy.burst.bot.model.Wallet;
 import io.enoy.burst.bot.model.WalletData;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -18,6 +19,9 @@ public class DebugDataGatherer implements WalletDataGatherer {
 	private static final Random RANDOM = new Random();
 	private final WalletService walletService;
 
+	@Value("${debug.random.increase:10}")
+	private double randomIncrease;
+
 	@Override
 	public List<WalletData> gatherDataOf(Collection<Wallet> wallets) {
 		List<WalletData> walletDataList = new ArrayList<>(wallets.size());
@@ -26,7 +30,7 @@ public class DebugDataGatherer implements WalletDataGatherer {
 			final Optional<WalletData> latestWalletDataOpt = walletService.getLatestWalletData(wallet);
 			final double previousPending = latestWalletDataOpt.map(WalletData::getPending).orElse(0d);
 
-			double newPending = previousPending + RANDOM.nextDouble() * 80;
+			double newPending = previousPending + RANDOM.nextDouble() * randomIncrease;
 			if(newPending > 100) { // payout at 100
 				newPending = 0;
 			}
